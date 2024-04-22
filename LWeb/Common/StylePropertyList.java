e05035a37bc33203d930014973230660d32d2d8c
Slymey <slymeye@gmail.com>
2024-04-22 21:06:42 +0200

    Updated cast for margin

--- LWeb/Common/StylePropertyList.java
+++ LWeb/Common/StylePropertyList.java
@@ -2,6 +2,7 @@ package LWeb.Common;
 
 import static LWeb.Common.Common.*;
 import LWeb.Common.StyleProperty.Property;
+import LWeb.Common.TypeProvider.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.function.BiFunction;
@@ -11,32 +12,54 @@ public class StylePropertyList {
     static ArrayList<StyleProperty> list = sg(()->{
         ArrayList<StyleProperty> al= new ArrayList<>();
         list=al;
-        al.add(new StyleProperty("margin-top", new TypeProvider.Length(0), null));
-        al.add(new StyleProperty("margin-right", new TypeProvider.Length(0), null));
-        al.add(new StyleProperty("margin-bottom", new TypeProvider.Length(0), null));
-        al.add(new StyleProperty("margin-left", new TypeProvider.Length(0), null));
-        al.add(new StyleProperty("margin", new TypeProvider.Length(0), sg(()->{
-            HashMap<TypeProvider[],BiFunction<TypeProvider[],Integer,Property[]>> casts = new HashMap<>();
-            casts.put(new TypeProvider[]{
-                new TypeProvider.Length(0)//margin: <length>
-            }, (TypeProvider[] t, Integer p) -> {
+        al.add(new StyleProperty("margin-top", new PropLength(0,"px"), null));
+        al.add(new StyleProperty("margin-right", new PropLength(0,"px"), null));
+        al.add(new StyleProperty("margin-bottom", new PropLength(0,"px"), null));
+        al.add(new StyleProperty("margin-left", new PropLength(0,"px"), null));
+        al.add(new StyleProperty("margin", new PropLength(0,"px"), sg(()->{
+            //<editor-fold defaultstate="collapsed" desc="cast">
+            BiFunction<TypeProvider[],Integer,Property[]> cast = (TypeProvider[] t, Integer p) -> {
+                if(t.length==0)
+                    return null;
+                if(t.length==1)
+                    return flatten(new Property[][]{
+                        getByName("margin-top",byi(t),p),
+                        getByName("margin-right",byi(t),p),
+                        getByName("margin-bottom",byi(t),p),
+                        getByName("margin-left",byi(t),p)
+                    });
+                if(t.length==2)
+                    return flatten(new Property[][]{
+                        getByName("margin-top",byi(t,0),p),
+                        getByName("margin-right",byi(t,1),p),
+                        getByName("margin-bottom",byi(t,0),p),
+                        getByName("margin-left",byi(t,1),p)
+                    });
+                if(t.length==3)
+                    return flatten(new Property[][]{
+                        getByName("margin-top",byi(t,0),p),
+                        getByName("margin-right",byi(t,1),p),
+                        getByName("margin-bottom",byi(t,0),p),
+                        getByName("margin-left",byi(t,2),p)
+                    });
                 return flatten(new Property[][]{
-                    getByName("margin-top",t,p),
-                    getByName("margin-right",t,p),
-                    getByName("margin-bottom",t,p),
-                    getByName("margin-left",t,p)
+                    getByName("margin-top",byi(t,0),p),
+                        getByName("margin-right",byi(t,1),p),
+                        getByName("margin-bottom",byi(t,2),p),
+                        getByName("margin-left",byi(t,3),p)
                 });
-            });
-            return casts;
+            };
+            return cast;
+            //</editor-fold>
         })));
-        al.add(new StyleProperty("width", new TypeProvider.Length(0), null));
-        al.add(new StyleProperty("height", new TypeProvider.Length(0), null));
-        al.add(new StyleProperty("background-color", new TypeProvider.ColorType(0), null));
+        al.add(new StyleProperty("width", new PropLength(0,"px"), null));
+        al.add(new StyleProperty("height", new PropLength(0,"px"), null));
+        al.add(new StyleProperty("background-color", new PropColor(0), null));
         
         return al;
     });
     
-    public static StyleProperty getByName(String name, int i){
+    public static StyleProperty getByName(String name){
         int ind = inList(name, list,(StyleProperty s)->{
             return s.name;
         },(String s, String s2)->{
@@ -51,7 +74,11 @@ public class StylePropertyList {
         },(String s, String s2)->{
             return s.compareTo(s2);
         });
-        if(ind<0)return null;
+        if(ind<0){
+            StyleProperty sc = new StyleProperty(name, tp[0], null);
+            list.add(sc);
+            return sc.getProperties(tp, priority);
+        }
         return list.get(ind).getProperties(tp, priority);
     }
 }
